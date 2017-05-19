f888537a08adde659335bc85ec985d626be92f15
wenhu.xwh <wenhu.xwh@alibaba-inc.com>
2017-05-19 15:41:59 +0800

    utility: move md5/crc/sha256 algorithm here.
    Signed-off-by: wenhu.xwh <wenhu.xwh@alibaba-inc.com>

--- framework/protocol/alink/system/system.mk
+++ framework/protocol/alink/system/system.mk
@@ -1,5 +1,5 @@
 NAME := system
 
-$(NAME)_SOURCES := alink_export.c config.c device.c crc.c 
+$(NAME)_SOURCES := alink_export.c config.c device.c 
 $(NAME)_INCLUDES := ../ ../accs/ ../../../../framework/connectivity/ 
-$(NAME)_INCLUDES += ../cota/ ../os/
+$(NAME)_INCLUDES += ../cota/ ../os/ ../../../../utility/digest_algorithm/
--- kernel/modules/kv/kv.mk
+++ kernel/modules/kv/kv.mk
@@ -1,10 +1,10 @@
 NAME := kv
 
 $(NAME)_SOURCES := kvmgr.c 
-$(NAME)_COMPONENTS += log hashtable protocol.alink.digest_algorithm
+$(NAME)_COMPONENTS += log hashtable digest_algorithm
 $(NAME)_COMPONENTS += protocol.alink.os
 $(NAME)_INCLUDES := ../../../framework/protocol/alink/os/
-$(NAME)_INCLUDES += ./include/ ../../../framework/protocol/alink/digest_algorithm/ 
+$(NAME)_INCLUDES += ../../../utility/digest_algorithm/ 
 $(NAME)_INCLUDES += ../../../framework/protocol/alink/system/ 
-$(NAME)_INCLUDES += ../../../utility/hashtable 
+$(NAME)_INCLUDES += ../../../utility/hashtable ./include 
 
