311dc620921f6125670491b7a05e624efe23183d
ruichao.lrc <ruichao.lrc@alibaba-inc.com>
2017-05-19 21:25:01 +0800

    [linuxhost]add board configuration for tfs

--- board/linuxhost/linuxhost.mk
+++ board/linuxhost/linuxhost.mk
@@ -6,3 +6,17 @@ HOST_ARCH           := linux
 HOST_MCU_FAMILY     := linux
 
 $(NAME)_COMPONENTS  := yloop vfs hal log vcall ysh alicrypto modules.kv mbedtls tfs
+
+CONFIG_LIB_TFS := y
+CONFIG_TFS_ID2_RSA := y
+CONFIG_TFS_ID2_3DES := n
+CONFIG_TFS_EMULATE := y
+CONFIG_TFS_ON_LINE := y
+CONFIG_TFS_OPENSSL := y
+CONFIG_TFS_MBEDTLS := n
+CONFIG_TFS_DEBUG := n
+CONFIG_TFS_VENDOR_FACTORY := csky
+CONFIG_TFS_VENDOR_VERSION := tee
+CONFIG_TFS_SE_LIB := libtfshal.a
+CONFIG_TFS_TEE := n
+CONFIG_TFS_TEST := y
