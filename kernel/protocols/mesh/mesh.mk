e8a6f038d1e41c6f254a2b09bb7d8c4388026224
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-05-19 17:22:35 +0800

    [mesh] file list into files.mk

--- kernel/protocols/mesh/mesh.mk
+++ kernel/protocols/mesh/mesh.mk
@@ -4,35 +4,7 @@ GLOBAL_INCLUDES += include
 
 $(NAME)_INCLUDES += include
 
-$(NAME)_SOURCES := src/core/umesh.c \
-                   src/core/mesh/mesh_mgmt.c \
-                   src/core/mesh/network_mgmt.c \
-                   src/core/mesh/link_mgmt.c \
-                   src/core/mesh/network_data.c \
-                   src/core/mesh/mesh_forwarder.c \
-                   src/core/mesh/address_mgmt.c \
-                   src/core/mesh/lowpan6.c \
-                   src/core/mesh/mcast.c \
-                   src/core/routing/router_mgr.c \
-                   src/core/routing/sid_router.c \
-                   src/core/routing/vector_router.c \
-                   src/core/routing/ssid_allocator.c \
-                   src/core/routing/rsid_allocator.c \
-                   src/core/security/dtls.c \
-                   src/core/security/keys_mgr.c \
-                   src/ipv6/lwip_adapter.c \
-                   src/ipv6/ip6_address.c \
-                   src/ipv6/lwip_ip6.c \
-                   src/hal/interfaces.c \
-                   src/hal/umesh_hal.c \
-                   src/utilities/encoding.c \
-                   src/utilities/message.c \
-                   src/utilities/timer.c \
-                   src/utilities/logging.c \
-                   src/utilities/mac_whitelist.c \
-                   src/utilities/memory.c \
-                   src/utilities/configs.c \
-                   src/tools/diags.c \
-                   src/tools/cli.c
+include kernel/protocols/mesh/files.mk
+$(NAME)_SOURCES := $(umesh_srcs)
 
 GLOBAL_DEFINES += CONFIG_YOS_MESH
