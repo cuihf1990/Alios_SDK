7b35c55eab228ee30584233873ed19655b02248e
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-05-27 14:12:03 +0800

    [ywss] remove dulipcate .c

--- framework/ywss/ywss.mk
+++ framework/ywss/ywss.mk
@@ -1,5 +1,5 @@
 NAME := ywss
-$(NAME)_SOURCES := awss.c enrollee.c sha256.c zconfig_utils.c enrollee.c zconfig_ieee80211.c
+$(NAME)_SOURCES := awss.c enrollee.c sha256.c zconfig_utils.c zconfig_ieee80211.c
 $(NAME)_SOURCES += zconfig_ut_test.c registrar.c zconfig_protocol.c zconfig_vendor_common.c
 $(NAME)_INCLUDES := ../protocol/alink/os/ ../protocol/alink/ ../../utility/base64/ ../protocol/alink/accs/
 $(NAME)_INCLUDES += ../protocol/alink/json/ ../connectivity/wsf/ ../../utility/digest_algorithm/
