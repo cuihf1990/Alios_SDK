83169c3e04a2d7ae5b25d247bb681c934dae7126
wenhu.xwh <wenhu.xwh@alibaba-inc.com>
2017-05-27 22:43:02 +0800

    ut:digest_algorithm: add digest_algorithm unit testcase.
    Signed-off-by: wenhu.xwh <wenhu.xwh@alibaba-inc.com>

--- example/yts/yts.mk
+++ example/yts/yts.mk
@@ -3,4 +3,8 @@ NAME := yts
 $(NAME)_SOURCES := main.c
 $(NAME)_COMPONENTS := testcase
 
+GLOBAL_CFLAGS  += -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
+GLOBAL_CFLAGS  += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized 
+GLOBAL_CFLAGS  += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable 
+GLOBAL_CFLAGS  +=  -Wno-unused-value -Wno-strict-aliasing
 GLOBAL_DEFINES += CONFIG_USE_DEF_AP
