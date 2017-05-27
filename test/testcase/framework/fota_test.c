97d832a102a6342580e758b8506b06ec4fa92a62
ting.guit <ting.guit@alibaba-inc.com>
2017-05-27 21:16:25 +0800

    yts case

--- test/testcase/framework/fota_test.c
+++ test/testcase/framework/fota_test.c
@@ -22,12 +22,15 @@
 
 #include <yunit.h>
 #include <yts.h>
-
+#include "ota_transport.h"
 extern void ota_check_update(const char *buf, int len);
-
 extern void ota_service_init(void);
-
 extern void do_update(const char *buf);
+extern void http_gethost_info(char* src, char* web, char* file, int* port);
+extern int http_socket_init(int port, char *host_addr);
+extern void ota_download_start(void * buf);
+extern int8_t parse_ota_requset(const char* request, int *buf_len, ota_request_params * request_parmas);
+extern int8_t parse_ota_response(const char* response, int buf_len, ota_response_params * response_parmas);
 
 const char *ota_info = "{\"md5\":\"6B21342306D0F619AF97006B7025D18A\",\"resourceUrl\":\"http://otalink.alicdn.com/ALINKTEST_LIVING_LIGHT_ALINK_TEST/v2.0.0.1/uthash-master.zip\",\"size\":\"265694 \",\"uuid\":\"5B7CFD5C6B1D6A231F5FB6B7DB2B71FD\",\"version\":\"v2.0.0.1\",\"zip\":\"0\"}";
 
@@ -37,6 +40,15 @@ static void test_fota_case(void)
     yos_post_event(EV_SYS, CODE_SYS_ON_START_FOTA, 0);
     ota_check_update("",1);
     do_update(ota_info);
+    do_update(NULL);
+
+    http_gethost_info(NULL, NULL, NULL, NULL);
+    http_socket_init(0, NULL);
+
+    ota_download_start(NULL);
+
+    parse_ota_requset(NULL, NULL, NULL);
+    parse_ota_response(NULL, 0, NULL);
 }
 
 static int init(void)
