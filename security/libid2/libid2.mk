4d3966e97045a49cd73d042aabe205986af8b16a
caidi.wcd <caidi.wcd@alibaba-inc.com>
2017-06-05 19:02:56 +0800

    add libid2 and libkm for armfhlinux

--- security/libid2/libid2.mk
+++ security/libid2/libid2.mk
@@ -5,8 +5,9 @@ $(NAME)_CFLAGS      += -Wall -Werror -Os
 GLOBAL_INCLUDES     += include
 
 ifeq ($(findstring linuxhost, $(BUILD_STRING)), linuxhost)
-$(NAME)_DEFINES          += CONFIG_LINUX_HOST
 $(NAME)_PREBUILT_LIBRARY := lib/linuxhost/libid2.a
+else ifeq ($(findstring armhflinux, $(BUILD_STRING)), armhflinux)
+$(NAME)_PREBUILT_LIBRARY := lib/armhflinux/libid2.a
 else
 $(error "not find correct platform!")
 endif
