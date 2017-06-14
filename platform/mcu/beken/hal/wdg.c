48f0a77453b9d48c785a5d94173d4d45ab1b8f52
杨诗伟 <yangsw@mxchip.com>
2017-06-14 15:31:42 +0800

    [moc108_hal] update boot.elf to support ota, add watchdog timer driver.

---
+++ platform/mcu/beken/hal/wdg.c
@@ -0,0 +1,24 @@
+#include "hal/soc/soc.h"
+#include "mico_rtos.h"
+#include "wdt_pub.h"
+#include "icu_pub.h"
+
+int32_t hal_wdg_init(uint32_t timeout)
+{
+    uint32_t para;
+
+    para = PWD_ARM_WATCHDOG_CLK_BIT;
+    icu_ctrl(CMD_CLK_PWR_UP, &para);
+    wdt_ctrl(WCMD_SET_PERIOD, &timeout);
+    return 0;
+}
+
+void hal_wdg_reload(void)
+{
+    wdt_ctrl(WCMD_CLEAR_COUNTER, 0);
+}
+
+int32_t hal_wdg_finalize(void)
+{
+    return 0;
+}
