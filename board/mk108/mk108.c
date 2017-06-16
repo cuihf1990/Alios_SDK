6d7aba15d9c44c25acc3651b05b2805d78448f2b
杨诗伟 <yangsw@mxchip.com>
2017-06-16 09:53:20 +0800

    [moc108_partition_modify] modify the partition.

--- board/mk108/mk108.c
+++ board/mk108/mk108.c
@@ -32,15 +32,23 @@ const hal_logic_partition_t hal_partitions[] =
 	    .partition_owner            = HAL_FLASH_EMBEDDED,
 	    .partition_description      = "Application",
 	    .partition_start_addr       = 0x12000,
-	    .partition_length           = 0x9E000,   //440k bytes
+	    .partition_length           = 0x9E000 + 0x40000,   //632k + 256k bytes
 	    .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
 	},
     [HAL_PARTITION_OTA_TEMP] =
     {
         .partition_owner           = HAL_FLASH_EMBEDDED,
         .partition_description     = "OTA Storage",
-        .partition_start_addr      = 0xB0000,
-        .partition_length          = 0x50000, //440k bytes
+        .partition_start_addr      = 0xB0000 + 0x40000,
+        .partition_length          = 0x50000 - 0x40000 - 0x1000, //320k - 256k - 4k bytes
         .partition_options         = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
+    },
+    [HAL_PARTITION_PARAMETER_3] =
+    {
+        .partition_owner            = HAL_FLASH_EMBEDDED,
+        .partition_description      = "PARAMETER3",
+        .partition_start_addr       = 0xFF000,
+        .partition_length           = 0x1000, //4k bytes
+        .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
 };
