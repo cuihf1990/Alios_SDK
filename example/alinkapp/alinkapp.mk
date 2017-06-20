1df15b871bb18f825e08701f75f5c0c30d5df70a
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-06-20 15:42:23 +0800

    [alinkapp] integreate gateway

--- example/alinkapp/alinkapp.mk
+++ example/alinkapp/alinkapp.mk
@@ -1,11 +1,17 @@
 NAME := alinkapp
 
+no_with_lwip := 1
 $(NAME)_SOURCES := alink_sample.c
-$(NAME)_COMPONENTS  := base64 hashtable log connectivity protocol.alink ywss modules.kv cli gateway
+$(NAME)_COMPONENTS := base64 hashtable log connectivity protocol.alink ywss modules.kv cli gateway
 $(NAME)_INCLUDES := ../../framework/protocol/alink/system/ ../../framework/protocol/alink/json/
-$(NAME)_INCLUDES += ../../kernel/modules/kv/include  
+$(NAME)_INCLUDES += ../../kernel/modules/kv/include
+$(NAME)_INCLUDES += ../../framework/gateway/
 GLOBAL_CFLAGS  += -Wall -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
 GLOBAL_CFLAGS  += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized 
 GLOBAL_CFLAGS  += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable 
 GLOBAL_CFLAGS  += -Wno-unused-value -Wno-strict-aliasing
 
+ifeq ($(DDA),1)
+GLOBAL_LDFLAGS += -lreadline -lncurses
+$(NAME)_COMPONENTS  += dda
+endif
