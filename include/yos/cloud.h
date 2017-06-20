21a804ffc0db7b534d9df481d7a43ffae23d28de
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-06-20 14:23:47 +0800

    [framework] added cloud component

---
+++ include/yos/cloud.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017 YunOS Project. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __YOS_CLOUD_H__
+#define __YOS_CLOUD_H__
+
+enum {
+    CLOUD_CONNECTED,
+    CLOUD_DISCONNECTED,
+    GET_DEVICE_STATUS,
+    SET_DEVICE_STATUS,
+    GET_DEVICE_RAWDATA,
+    SET_DEVICE_RAWDATA,
+    UPGRADE_DEVICE,
+    CANCEL_UPGRADE_DEVICE,
+    GET_SUB_DEVICE_STATUS,
+    SET_SUB_DEVICE_STATUS,
+    MAX_EVENT_TYPE,
+};
+
+typedef void (*yos_cloud_cb_t)(int event, const char *json_buffer);
+int yos_cloud_register_callback(int cb_type, yos_cloud_cb_t cb);
+int yos_cloud_report(const char *method,
+                     const char *json_buffer,
+                     void (*done_cb)(void *),
+                     void *arg);
+
+void yos_cloud_trigger(int cb_type, const char *json_buffer);
+void yos_cloud_register_backend(int (*report)(const char *method, const char *json_buffer));
+#endif
