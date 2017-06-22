5754ddee9462b37f2b8c5afdbfff6c483150394b
Li Cheng <lc122798@alibaba-inc.com>
2017-06-22 21:52:12 +0800

    [mk108] fixed mk108 STDIO options

--- board/mk108/mk108.mk
+++ board/mk108/mk108.mk
@@ -32,6 +32,7 @@ HOST_MCU_FAMILY      := beken
 $(NAME)_SOURCES := board.c
 
 GLOBAL_INCLUDES += .
+GLOBAL_DEFINES += STDIO_UART=1
 
 CURRENT_TIME = $(shell date +%Y%m%d.%H%M)
 define get-os-version
