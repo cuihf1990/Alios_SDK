49b713ee305e98c85a0c0935b787981d29aa76a7
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-06-24 16:40:15 +0800

    [alink] enable wsf/ywss log

--- framework/ywss/ywss.mk
+++ framework/ywss/ywss.mk
@@ -3,5 +3,6 @@ $(NAME)_SOURCES := awss.c enrollee.c sha256.c zconfig_utils.c zconfig_ieee80211.
 $(NAME)_SOURCES += zconfig_ut_test.c registrar.c zconfig_protocol.c zconfig_vendor_common.c
 $(NAME)_INCLUDES := ../protocol/alink/os/ ../protocol/alink/ ../../utility/base64/ ../protocol/alink/accs/
 $(NAME)_INCLUDES += ../protocol/alink/json/ ../connectivity/wsf/ ../../utility/digest_algorithm/
+$(NAME)_DEFINES += DEBUG
 
 GLOBAL_DEFINES += CONFIG_YWSS
