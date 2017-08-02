0adfd3b6910fded634650f3bfe98be4a66452566
luwang <wanglu.luwang@alibaba-inc.com>
2017-08-02 16:28:57 +0800

    Add mutex to mesh memory

--- kernel/protocols/mesh/src/utilities/memory.c
+++ kernel/protocols/mesh/src/utilities/memory.c
@@ -20,26 +20,51 @@
 #include <yos/kernel.h>
 #include "umesh_utils.h"
 
-static ur_mem_stats_t g_stats;
+typedef struct mem_info_s {
+    ur_mem_stats_t stats;
+    yos_mutex_t mutex;
+} mem_info_t;
+static mem_info_t g_mem_info;
 
 void *ur_mem_alloc(uint16_t size)
 {
-    void *mem;
+    void *mem = NULL;
+
+    if (g_mem_info.stats.num + size > MEM_BUF_SIZE) {
+        return mem;
+    }
 
     mem = (void *)yos_malloc((size_t)size);
     if (mem) {
-        g_stats.num += size;
+        yos_mutex_lock(&g_mem_info.mutex, YOS_WAIT_FOREVER);
+        g_mem_info.stats.num += size;
+        yos_mutex_unlock(&g_mem_info.mutex);
     }
     return mem;
 }
 
 void ur_mem_free(void *mem, uint16_t size)
 {
-    g_stats.num -= size;
-    yos_free(mem);
+    if (mem) {
+        yos_free(mem);
+        yos_mutex_lock(&g_mem_info.mutex, YOS_WAIT_FOREVER);
+        g_mem_info.stats.num -= size;
+        yos_mutex_unlock(&g_mem_info.mutex);
+    }
+}
+
+void umesh_mem_init(void)
+{
+    bzero(&g_mem_info.stats, sizeof(g_mem_info.stats));
+    yos_mutex_new(&g_mem_info.mutex);
+}
+
+void umesh_mem_deinit(void)
+{
+    yos_mutex_free(&g_mem_info.mutex);
 }
 
 const ur_mem_stats_t *ur_mem_get_stats(void)
 {
-    return &g_stats;
+    return &g_mem_info.stats;
 }
