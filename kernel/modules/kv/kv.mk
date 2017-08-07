1df6b5d92ed36346d9caf6a3074d2b8293c3ed85
woksd <keepwalking.zeng@gmail.com>
2017-08-07 17:18:22 +0100

    rm useless include for utility is global

--- kernel/modules/kv/kv.mk
+++ kernel/modules/kv/kv.mk
@@ -2,7 +2,5 @@ NAME := kv
 
 $(NAME)_SOURCES := kvmgr.c 
 $(NAME)_COMPONENTS += log hashtable digest_algorithm
-$(NAME)_INCLUDES += ../../../utility/digest_algorithm/ 
-$(NAME)_INCLUDES += ../../../utility/hashtable 
 
 GLOBAL_INCLUDES  += include
