0f39f8ea1e5811e3337dca5a59c0ea2ea28c9a23
Li Cheng <lc122798@alibaba-inc.com>
2017-08-15 14:45:10 +0800

    [testbed] add automatic tests: tree_topology and multicast

--- tools/testbed/autoscripts/example_test.py
+++ tools/testbed/autoscripts/example_test.py
@@ -1,20 +1,21 @@
 import sys, time
-sys.path.append("../")
+sys.path.append('../')
 from autotest import Autotest
 
-devices = {'A':"mxchip-DN02QYH3", "B":"mxchip-DN02QYHF", "C":"mxchip-DN02QYHK"}
-logname=time.strftime("%Y-%m-%d@%H-%M")
-logname = "example-" + logname +".log"
+devices = {'A':'mxchip-DN02QYHE', 'B':'mxchip-DN02QYI2', 'C':'mxchip-DN02QYI9'}
+logname=time.strftime('%Y-%m-%d@%H-%M')
+logname = 'example-' + logname +'.log'
 at=Autotest()
-at.start("10.125.52.132", 34568, logname)
+at.start('10.125.52.132', 34568, logname)
 
 at.device_subscribe(devices)
 at.device_run_cmd('A', ['netmgr', 'clear'])
 at.device_run_cmd('A', ['reboot'])
-time.sleep(2)
-at.device_run_cmd('A', ['netmgr', 'connect', 'Alibaba_test_wb065324TP_LINK', 'aliyunos_1234'])
+time.sleep(5)
+at.device_run_cmd('A', ['netmgr', 'connect', 'wuchen_test', 'aliyunos'])
 time.sleep(8)
-print at.device_run_cmd('A', ['umesh', 'state'], 1, 0.5)
+filter = ['disabled', 'detached', 'attached', 'leaf', 'router', 'super_router', 'leader', 'unknown']
+print at.device_run_cmd('A', ['umesh', 'state'], 1, 0.5, filter)
 
 at.stop()
 
