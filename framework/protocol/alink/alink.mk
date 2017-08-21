cef1db567dbca7770d2f167aa3cffb0663289b0d
woksd <keepwalking.zeng@gmail.com>
2017-08-21 17:01:15 +0100

    reduce dependency between alink and gateway

--- framework/protocol/alink/alink.mk
+++ framework/protocol/alink/alink.mk
@@ -9,4 +9,3 @@ include framework/protocol/alink/system/system.mk
 include framework/protocol/alink/cota/cota.mk
 include framework/protocol/alink/os/os.mk
 
-$(NAME)_COMPONENTS  :=  gateway.devmgr gateway.msdp
