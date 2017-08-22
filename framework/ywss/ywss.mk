4acc1bc6eade35e3e39655f1b5ee8e08be2755a7
Wenjun Chen <wenjunchen.cwj@alibaba.com>
2017-08-22 09:29:08 +0800

    [ywss] Remove dependency of gateway.

--- framework/ywss/ywss.mk
+++ framework/ywss/ywss.mk
@@ -4,8 +4,6 @@ GLOBAL_INCLUDES += .
 $(NAME)_SOURCES := awss.c enrollee.c sha256.c zconfig_utils.c zconfig_ieee80211.c wifimgr.c ywss_utils.c
 $(NAME)_SOURCES += zconfig_ut_test.c registrar.c zconfig_protocol.c zconfig_vendor_common.c
 
-$(NAME)_INCLUDES += ../gateway/
-
 $(NAME)_DEFINES += DEBUG
 
 GLOBAL_DEFINES += CONFIG_YWSS
