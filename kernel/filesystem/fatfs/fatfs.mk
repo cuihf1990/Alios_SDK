4c4bca40fc380222f837679c3a2071d132f3ce35
Fan Qian <pigmike1018@126.com>
2017-08-22 17:18:10 +0800

    [Fatfs] commit Fatfs 0.13R open source code 	remove some language unicode tables, old retain U.S. and Simplified Chinese

---
+++ kernel/filesystem/fatfs/fatfs.mk
@@ -0,0 +1,10 @@
+NAME := fatfs
+
+$(NAME)_TYPE        := kernel
+
+$(NAME)_SOURCES     := ff.c
+$(NAME)_SOURCES     += ffsystem.c
+$(NAME)_SOURCES     += fatfs.c
+$(NAME)_SOURCES     += ffunicode.c
+
+GLOBAL_INCLUDES     += include 
