842f2a7f3fce72335612edf5f4c575d6cc7579af
刘自龙(061089) <zilong.liuzl@alibaba-inc.com>
2017-08-22 20:36:49 +0800

    [BINS]: add yos.h for app

---
+++ include/yos/yos.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 YunOS Project. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __YOS_H__
+#define __YOS_H__
+
+#include <yos/kernel.h>
+#include <yos/alink.h>
+#include <yos/cli.h>
+#include <yos/cloud.h>
+#include <yos/debug.h>
+#include <yos/framework.h>
+#include <yos/list.h>
+#include <yos/log.h>
+#include <yos/network.h>
+#include <yos/types.h>
+#include <yos/version.h>
+
+#endif
+
