0d4e28f7ab9d3dbf5f0c241eb264e8e8350738a8
刘自龙(061089) <zilong.liuzl@alibaba-inc.com>
2017-08-24 12:23:12 +0800

    [CODE-STYLE]: modify yos.c style

--- platform/mcu/beken/yos/yos.c
+++ platform/mcu/beken/yos/yos.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 YunOS Project. All rights reserved.
+ * Copyright (C) 2017 YunOS Project. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,22 +22,26 @@
 
 ktask_t *g_yos_init;
 
+extern void board_init(void);
+
+extern int application_start(int argc, char **argv);
+
 #ifdef YOS_BINS
 extern void *g_syscall_tbl[];
-extern int application_start(int argc, char **argv);
-extern void board_init(void);
-extern char app_info_addr;
-extern k_mm_head *g_kmm_head;
+extern char  app_info_addr;
+
+extern k_mm_head  *g_kmm_head;
+
 struct app_info_t *app_info = (struct app_info_t *)&app_info_addr;
 
 static void application_init(void)
 {
     memcpy((void *)(app_info->data_ram_start), (void *)(app_info->data_flash_begin),
-            app_info->data_ram_end - app_info->data_ram_start);
+           app_info->data_ram_end - app_info->data_ram_start);
     memset((void *)(app_info->bss_start), 0, app_info->bss_end - app_info->bss_start);
 
     yunos_add_mm_region(g_kmm_head, (void *)(app_info->heap_start),
-            app_info->heap_end - app_info->heap_start);
+                        app_info->heap_end - app_info->heap_start);
 
     yunos_mm_leak_region_init((void *)(app_info->data_ram_start), (void *)(app_info->data_ram_end));
     yunos_mm_leak_region_init((void *)(app_info->bss_start), (void *)(app_info->bss_end));
@@ -59,17 +63,20 @@ static void yos_init(void)
     vfs_init();
     vfs_device_init();
 
-    for(i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         vflash_register_partition(i);
     }
 
     yos_cli_init();
     yos_kv_init();
     yos_loop_init();
+
 #ifdef YOS_BINS
     application_init();
-    if(app_info->app_entry)
+
+    if (app_info->app_entry) {
         app_info->app_entry((int)g_syscall_tbl, NULL);
+    }
 #else
     application_start(0, NULL);
 #endif
