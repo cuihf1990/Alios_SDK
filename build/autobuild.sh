b766da84e684473e84419e78b3ccf41039914aa4
刘自龙(061089) <zilong.liuzl@alibaba-inc.com>
2017-08-25 18:56:22 +0800

    [COPYRIGHT]: add copyright.py to autobuild.sh

--- build/autobuild.sh
+++ build/autobuild.sh
@@ -78,3 +78,6 @@ echo "build ${branch} branch succeed"
 echo "----------check CODE-STYLE now----------"
 ./build/astyle.sh
 echo "----------------------------------------"
+echo "----------check COPYRIGHT now-----------"
+python ./build/copyright.py
+echo "----------------------------------------"
