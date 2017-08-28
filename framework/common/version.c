08d510bade6822ee144794c4afdb8a169beb5412
gui ting <ting.guit@alibaba-inc.com>
2017-08-28 21:45:01 +0800

    add bins version

--- framework/common/version.c
+++ framework/common/version.c
@@ -20,6 +20,7 @@
 */
 #include <string.h>
 #include <yos/log.h>
+#include<stdio.h>
 
 #ifndef YOS_EXPORT
 #define YOS_EXPORT
@@ -33,26 +34,39 @@
 
 YOS_EXPORT YOS_WEAK const char   *get_yos_product_model(void)
 {
-    char *ret = SYSINFO_PRODUCT_MODEL;
-    return (const char *)ret;
+    return (const char *)SYSINFO_PRODUCT_MODEL;
 }
-
 
 YOS_EXPORT YOS_WEAK const char   *get_yos_device_name(void)
 {
-    char *ret = SYSINFO_DEVICE_NAME;
-    return (const char *)ret;
+    return (const char *)SYSINFO_DEVICE_NAME;
 }
 
 YOS_EXPORT YOS_WEAK const char   *get_yos_os_version(void)
 {
-    char *ret = SYSINFO_OS_VERSION;
-    return (const char *)ret;
+    return (const char *)SYSINFO_OS_VERSION;
 }
+
+#ifdef SYSINFO_OS_BINS
+YOS_EXPORT YOS_WEAK const char   *get_yos_kernel_version(void)
+{
+    return (const char *)SYSINFO_KERNEL_VERSION;
+}
+
+
+YOS_EXPORT YOS_WEAK const char   *get_yos_app_version(void)
+{
+    return (const char *)SYSINFO_APP_VERSION;
+}
+#endif
 
 YOS_EXPORT YOS_WEAK void dump_sys_info(void)
 {
     LOGI(TAG, "os_version: %s", get_yos_os_version());
+    #ifdef SYSINFO_OS_BINS
+    LOGI(TAG, "kernel_version: %s", get_yos_kernel_version());
+    LOGI(TAG, "app_version: %s", get_yos_app_version());
+    #endif
     LOGI(TAG, "product_model: %s", get_yos_product_model());
     LOGI(TAG, "device_name: %s", get_yos_device_name());
 }
