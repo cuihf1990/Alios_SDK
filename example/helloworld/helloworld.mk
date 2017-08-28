3bf98c82a7c619dce65a1e362e6093c3630a64f6
刘自龙(061089) <zilong.liuzl@alibaba-inc.com>
2017-08-28 19:28:29 +0800

    [LICENSE]: delete license of ./example

--- example/helloworld/helloworld.mk
+++ example/helloworld/helloworld.mk
@@ -1,19 +1,10 @@
-#
-#  UNPUBLISHED PROPRIETARY SOURCE CODE
-#  Copyright (c) 2016 MXCHIP Inc.
-#
-#  The contents of this file may not be disclosed to third parties, copied or
-#  duplicated in any form, in whole or in part, without the prior written
-#  permission of MXCHIP Corporation.
-#
-
 NAME := helloworld
 
 $(NAME)_SOURCES := helloworld.c
 
 $(NAME)_COMPONENTS += cli
 
-GLOBAL_DEFINES      += YOS_NO_WIFI
+GLOBAL_DEFINES += YOS_NO_WIFI
 
 ifeq ($(BENCHMARKS),1)
 $(NAME)_COMPONENTS  += benchmarks
