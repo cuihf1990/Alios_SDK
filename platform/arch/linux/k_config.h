2ee8ac61f4782f01acd2a373dabd87025176821f
lingjun chen <lingjun.clj@alibaba.com>
2017-08-31 03:07:15 -0700

    set YUNOS_CONFIG_CPU_NUM 2 for linux host

--- platform/arch/linux/k_config.h
+++ platform/arch/linux/k_config.h
@@ -102,7 +102,7 @@
 /* kernel trace conf */
 #define YUNOS_CONFIG_TRACE                   1
 
-#define YUNOS_CONFIG_CPU_NUM                 1
+#define YUNOS_CONFIG_CPU_NUM                 2
 
 #endif /* K_CONFIG_H */
 
