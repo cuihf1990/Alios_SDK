dcc3fd1d01c71c349ed92ee9b2b74b7e0c44eccc
Zilong Liu <liuziloong@gmail.com>
2017-09-13 17:30:09 +0800

    [ANNOTATION]: modify alink.h

--- include/aos/alink.h
+++ include/aos/alink.h
@@ -5,160 +5,94 @@
 #ifndef _ALINK_EXPORT_H_
 #define _ALINK_EXPORT_H_
 
-#if defined(__cplusplus) /* If this is a C++ compiler, use C linkage */
+#if defined(__cplusplus)
 extern "C"
 {
 #endif
 
-/** @defgroup wifi_device wifi device API
- *  @{
- */
-
-
-/** @defgroup wifi_debug debug
- *  @{
- */
-
-/**
- * log level def.
- */
 enum ALINK_LOG_LEVEL {
-    ALINK_LL_NONE, /**< disable log */
-    ALINK_LL_FATAL, /**< fatal error log will output */
-    ALINK_LL_ERROR, /**< error + fatal log will output */
-    ALINK_LL_WARN, /**< warn + error + fatal log will output(default level) */
-    ALINK_LL_INFO, /**< info + warn + error + fatal log will output */
-    ALINK_LL_DEBUG, /**< debug + info + warn + error + fatal log will output */
-    ALINK_LL_TRACE, /**< trace + debug + info + warn + error + fatal will output */
+    ALINK_LL_NONE,  /* disable log */
+    ALINK_LL_FATAL, /* fatal error log will output */
+    ALINK_LL_ERROR, /* error + fatal log will output */
+    ALINK_LL_WARN,  /* warn + error + fatal log will output(default level) */
+    ALINK_LL_INFO,  /* info + warn + error + fatal log will output */
+    ALINK_LL_DEBUG, /* debug + info + warn + error + fatal log will output */
+    ALINK_LL_TRACE, /* trace + debug + info + warn + error + fatal will output */
 };
 
 /**
- * @brief log level control
+ * Log level control
  *
- * @param[in] loglevel
- * @return None.
- * @see enum ALINK_LOG_LEVEL.
- * @note None.
+ * @param[in]  loglevel  ALINK_LOG_LEVEL
  */
 void alink_set_loglevel(enum ALINK_LOG_LEVEL loglevel);
 
 /**
- * @brief enable sandbox mode, for debug
+ * Enable sandbox mode, for debug
  *
- * @param None.
- * @retval 0 on success, otherwise -1 will return
- * @see None.
- * @note None.
+ * @return  0 on success, otherwise -1 will return
  */
 int alink_enable_sandbox_mode(void);
 
 /**
- * @brief enable daily mode, for debug
+ * Enable daily mode, for debug
  *
- * @param None.
- * @retval 0 on success, otherwise -1 will return
- * @see None.
- * @note None.
+ * @param[in]  server_ip  IP Addr
+ * @param[in]  port       server port
+ *
+ * @return  0 on success, otherwise -1 will return
  */
 int alink_enable_daily_mode(const char *server_ip, int port);
 
-/** @} */ //end of debug
-
-
-
-/** @defgroup wifi_entry main
- *  @{
- */
-
 /**
- * @brief entry function. start alink gateway service.
+ * Entry function. start alink gateway service.
  *
- * @param None.
- * @retval 0 on success, otherwise -1 will return
- * @see None.
- * @note None.
+ * @return  0 on success, otherwise -1 will return
  */
 int alink_start(void);
 
-#define ALINK_WAIT_FOREVER      (0)
+#define ALINK_WAIT_FOREVER (0)
+
 /**
- * @brief waiting alink connect to aliyun server
+ * Waiting alink connect to aliyun server
  *
- * @param[in] timeout_ms: time in milliseconds,
- *              use ALINK_WAIT_FOREVER to wait until aliyun server is connected
- * @retval 0 when connect to server successfully, otherwise -1 will return
- * @see None.
- * @note None.
+ * @param[in] timeout_ms: time in milliseconds, use ALINK_WAIT_FOREVER to wait until server is connected
+ *
+ * @return  0 when connect to server successfully, otherwise -1 will return
  */
 int alink_wait_connect(int timeout_ms);
 
 /**
- * @brief destroy alink service and free resources
+ * Destroy alink service and free resources
  *
- * @param None.
- * @retval 0 on success, otherwise -1 will return
- * @see None.
- * @note this func will block at most 15 seconds to
- *      stop all alink related process(thread)
+ * @return  0 on success, otherwise -1 will return
+ *
+ * @note  this func will block at most 15 seconds to stop all alink related process(thread)
  */
 int alink_end(void);
 
 /**
- * @brief reset user account binding.
+ * Reset user account binding.
  *
- * @retval 0 on success, -1 when params invalid
- * @see None.
- * @note None.
+ * @return  0 on success, -1 when params invalid
  */
 int alink_factory_reset(void);
 
-/** @} */ //end of entry
-
-/** @defgroup wifi_report report status
- *  @{
- */
-
-/* TODO: tobe removed
-    int alink_get_sub_device_key(char *id, char *mode, char *rkey, char *ckey, char *dkey);
-
-    int alink_backup_data(char *id, char *key, char *val);
-    int alink_retrieve_data(char *id, char *key, char *val, int *val_len);
-    int alink_sync_device_status(char *id, char *buf, int *buf_len);
-    int alink_register_remote_service(alink_up_cmd_ptr cmd);
-    int alink_unregister_remote_service(alink_up_cmd_ptr cmd);
-    int alink_post_remote_service_rsp(alink_up_cmd_ptr cmd);
-    int alink_post_relate_devices(alink_up_cmd_ptr cmd);
-    int alink_post_device_data_array(alink_up_cmd_ptr cmd);
-    unsigned int alink_get_time(void);
-*/
-
 /**
- * @brief report alink message, it is a fundamental func.
+ * Report alink message, it is a fundamental func.
  *
- * @param[in] method: alink protocol method,
- *          i.e. "postDeviceRawData", "retrieveDeviceData".
- * @param[in] json_buffer: json format buffer, like {"OnOff":"1", "Light":"80"}.
- * @retval 0 when successfully got response from cloud,
+ * @param[in]  method       alink protocol method, i.e. "postDeviceRawData", "retrieveDeviceData".
+ * @param[in]  json_buffer  json format buffer, like {"OnOff":"1", "Light":"80"}.
+ *
+ * @return  0 when successfully got response from cloud,
  *          otherwise this func will block until timeout and -1 will return
- * @see None.
- * @note when connection with server is unstable, this func will block
- *      until got response from server or timeout.
+ *
+ * @note when connection with server is unstable, this func will block until got response from server or timeout.
  */
 int alink_report(const char *method, char *json_buffer);
 
 int alink_report_rawdata(const char *rawdata, int len);
 
-//int alink_post_device_data_with_ack(alink_up_cmd_ptr cmd);//FIXME
-
-/** @} */ //end of report
-
-/** @defgroup wifi_callback    callback
- *  @{
- */
-
-/**
- * alink callback event
- */
 enum ALINK_WIFI_CALLBACK {
     /**
      * void callback_cloud_connected(void)
@@ -201,42 +135,28 @@ enum ALINK_WIFI_CALLBACK {
  * @param[in] cb_func: callback func pointer, func declaration see related comments.
  *
  * @retval  0 on success, otherwise -1 will return
- * @see enum ALINK_WIFI_CALLBACK.
- * @note None.
  */
 int alink_register_callback(unsigned char cb_type, void *cb_func);
-/** @} */ //end of callback
-
-/** @defgroup wifi_awss awss
- *  @{
- */
 
 /**
- * @brief start awss service, block method,
- *      block until awss succeed, or timeout(see Note).
+ * Start awss service, block method, block until awss succeed, or timeout(see Note).
  *
- * @return 0 on success, otherwise non-zero value will return
-   @verbatim
-     = 0: connect AP & DHCP success
-     = -1: get ssid & passwd fail
-     = -2: connect AP / DHCP fail
-   @endverbatim
- * @see None.
- * @note platform_awss_get_timeout_interval_ms() return monitor timeout interval,
- *      AP connection timeout is 30s.
+ * @return  0 on success, otherwise non-zero value will return
+ *    =  0: connect AP & DHCP success
+ *    = -1: get ssid & passwd fail
+ *    = -2: connect AP / DHCP fail
+ *
+ * @note  platform_awss_get_timeout_interval_ms() return monitor timeout interval,
+ *        AP connection timeout is 30s.
  */
 int awss_start(void);
 
 /**
- * @brief calling this func force awss_start exit.
+ * Calling this func force awss_start exit.
  *
- * @param None.
- * @retval  0 on success, otherwise -1 will return
- * @see None.
- * @note None.
+ * @return  0 on success, otherwise -1 will return
  */
 int awss_stop(void);
 
-/** @} */ //end of awss
-
 #endif
+
