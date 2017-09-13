f7349709dc8454d06777d553e2b1fe14a83d2719
Fan Qian <pigmike1018@126.com>
2017-09-13 16:32:16 +0800

    Update kv.h annotation

--- include/aos/kv.h
+++ include/aos/kv.h
@@ -13,25 +13,25 @@ extern "C"
 #endif
 
 /**
- * @brief add another KV pair.
+ * Add another KV pair.
  *
- * @param[in]  @key    the key of the KV pair.
- * @param[in]  @value  the value of the KV pair.
- * @param[in]  @len    the length of the @value.
- * @param[in]  @sync   1: save the KV pair to flash right now,
- *                     0: do not save the pair this time.
+ * @param[in]  key    the key of the KV pair.
+ * @param[in]  value  the value of the KV pair.
+ * @param[in]  len    the length of the value.
+ * @param[in]  sync   1: save the KV pair to flash right now,
+ *                    0: do not save the pair this time.
  *
  * @retval  0 on success, otherwise -1 will be returned
  */
 int aos_kv_set(const char *key, const void *value, int len, int sync);
 
 /**
- * @brief get the KV value stored in @buffer by its key @key.
+ * Get the KV value stored in buffer by its key.
  *
- * @param[in]   @key       the key of the KV you want to get.
- * @param[out]  @buffer    the memory to store KV value.
- * @param[out]  @buff_len  the real lenght of value.
- * @note: the @buffer_len should be large enough to store the value,
+ * @param[in]   @key         the key of the KV you want to get.
+ * @param[out]  @buffer      the memory to store KV value.
+ * @param[out]  @buffer_len  the real lenght of value.
+ * @note: the buffer_len should be large enough to store the value,
  *            otherwise @buffer would be NULL.
  *
  * @retval  0 on success, otherwise -1 will be returned
@@ -39,9 +39,9 @@ int aos_kv_set(const char *key, const void *value, int len, int sync);
 int aos_kv_get(const char *key, void *buffer, int *buffer_len);
 
 /**
- * @brief delete the KV pair by its key @key.
+ * Delete the KV pair by its key.
  *
- * @param[in]  @key  the key of the KV pair you want to delete.
+ * @param[in]  key  the key of the KV pair you want to delete.
  *
  * @retval  0 on success, otherwise -1 will be returned
  */
