adf4a742c12142229e7622f457a79d1fc0f60495
Fan Qian <pigmike1018@126.com>
2017-09-19 20:19:41 +0800

    update kv.h & vfs.h annotation

--- include/aos/kv.h
+++ include/aos/kv.h
@@ -13,36 +13,37 @@ extern "C"
 #endif
 
 /**
- * Add another KV pair.
+ * Add a new KV pair.
  *
  * @param[in]  key    the key of the KV pair.
  * @param[in]  value  the value of the KV pair.
  * @param[in]  len    the length of the value.
- * @param[in]  sync   save the KV pair to flash right now (should always be 1)
+ * @param[in]  sync   save the KV pair to flash right now (should always be 1).
  *
- * @retval  0 on success, negative error on failure
+ * @retval  0 on success, negative error on failure.
  */
 int aos_kv_set(const char *key, const void *value, int len, int sync);
 
 /**
- * Get the KV value stored in buffer by its key.
+ * Get the KV pair's value stored in buffer by its key.
  *
- * @param[in]   key         the key of the KV you want to get.
- * @param[out]  buffer      the memory to store KV value.
- * @param[out]  buffer_len  the real length of value.
- * @note: the buffer_len should be large enough to store the value,
- *            otherwise buffer would be NULL.
+ * @param[in]      key         the key of the KV pair to get.
+ * @param[out]     buffer      the memory to store the value.
+ * @param[in-out]  buffer_len  in: the length of the input buffer.
+ *                             out: the real length of the value.
+ * @note: the buffer_len should be larger than the real length of the value,
+ *        otherwise buffer would be NULL.
  *
- * @retval  0 on success, negative error on failure
+ * @retval  0 on success, negative error on failure.
  */
 int aos_kv_get(const char *key, void *buffer, int *buffer_len);
 
 /**
  * Delete the KV pair by its key.
  *
- * @param[in]  key  the key of the KV pair you want to delete.
+ * @param[in]  key  the key of the KV pair to delete.
  *
- * @retval  0 on success, negative error on failure
+ * @retval  0 on success, negative error on failure.
  */
 int aos_kv_del(const char *key);
 
--- include/aos/vfs.h
+++ include/aos/vfs.h
@@ -18,170 +18,170 @@ typedef struct {
 } aos_dirent_t;
 
 typedef struct {
-    int dd_vfs_fd; /* This keeps track of the current directory position for telldir */
+    int dd_vfs_fd;
     int dd_rsv;
 } aos_dir_t;
 
 /**
- * Open the file or device by its path
+ * Open the file or device by its path.
  *
- * @param[in]  path   the path of the file or device you want to open
- * @param[in]  flags  the mode of open operation
+ * @param[in]  path   the path of the file or device to open.
+ * @param[in]  flags  the mode of open operation.
  *
- * @return  the new file descriptor, negative error on failure
+ * @return  the new file descriptor, negative error on failure.
  */
 int aos_open(const char *path, int flags);
 
 /**
- * Close the file or device by its fd
+ * Close the file or device by its file descriptor.
  *
- * @param[in]  fd  the handle of the file or device
+ * @param[in]  fd  the file descriptor of the file or device.
  *
- * @return  0 on success, negative error on failure
+ * @return  0 on success, negative error on failure.
  */
 int aos_close(int fd);
 
 /**
- * Read the contents of a file or device into a buffer
+ * Read the contents of a file or device into a buffer.
  *
- * @param[in]   fd      the handle of the file or device
- * @param[in]   nbytes  the number of bytes to read
- * @param[out]  buf     the buffer to read in to
+ * @param[in]   fd      the file descriptor of the file or device.
+ * @param[in]   nbytes  the number of bytes to read.
+ * @param[out]  buf     the buffer to read in to.
  *
- * @return  The number of bytes read, 0 at end of file, negative error on failure
+ * @return  The number of bytes read, 0 at end of file, negative error on failure.
  */
 ssize_t aos_read(int fd, void *buf, size_t nbytes);
 
 /**
- * Write the contents of a buffer to file or device
+ * Write the contents of a buffer to file or device.
  *
- * @param[in]  fd      the handle of the file or device
- * @param[in]  nbytes  the number of bytes to write
- * @param[in]  buf     the buffer to write from
+ * @param[in]  fd      the file descriptor of the file or device.
+ * @param[in]  nbytes  the number of bytes to write.
+ * @param[in]  buf     the buffer to write from.
  *
- * @return  The number of bytes written, negative error on failure
+ * @return  The number of bytes written, negative error on failure.
  */
 ssize_t aos_write(int fd, const void *buf, size_t nbytes);
 
 /**
- * This is a wildcard API for sending controller specific commands
+ * This is a wildcard API for sending controller specific commands.
  *
- * @param[in]  fd   the handle of the file or device
- * @param[in]  cmd  A controller specific command
- * @param[in]  arg  Argument to the command; interpreted according to the command
+ * @param[in]  fd   the file descriptior of the file or device.
+ * @param[in]  cmd  A controller specific command.
+ * @param[in]  arg  Argument to the command, interpreted according to the command.
  *
- * @return  any return from the command
+ * @return  any return from the command.
  */
 int aos_ioctl(int fd, int cmd, unsigned long arg);
 
 /**
- * A mechanism to multiplex input/output over a set of file handles(file descriptors).
- * For every file handle provided, poll() examines it for any events registered for that particular file handle.
+ * A mechanism to multiplex input/output over a set of file descriptors.
+ * For every file descriptor provided, poll() examines it for any events registered for that particular file descriptor.
  *
- * @param[in]  fds      a point to the array of PollFh struct carrying a FileHandle and bitmasks of events
- * @param[in]  nfhs     number of file handles
- * @param[in]  timeout  timer value to timeout or -1 for loop forever
+ * @param[in]  fds      a point to the array of pollfd struct carrying a file descriptor and bitmasks of events.
+ * @param[in]  nfhs     number of file descriptors.
+ * @param[in]  timeout  timer value to timeout or -1 for loop forever.
  *
- * @return  number of file handles selected (for which revents is non-zero). 0 if timed out with nothing selected. -1 for error.
+ * @return  number of file descriptors selected (for which revents is non-zero). 0 if timed out with nothing selected. -1 for error.
  */
 int aos_poll(struct pollfd *fds, int nfds, int timeout);
 
 /**
  * Performs one of the operations described below on the open file descriptor, The operation is determined by cmd.
  *
- * @param[in]  fd   the handle of the file or device
- * @param[in]  cmd  the operation of the file or device
- * @param[in]  val  it depends on whether cmd need params
+ * @param[in]  fd   the file descriptor of the file or device.
+ * @param[in]  cmd  the operation of the file or device.
+ * @param[in]  val  it depends on whether cmd need params.
  *
- * @return  0 on success, negative error on failure
+ * @return  0 on success, negative error on failure.
  */
 int aos_fcntl(int fd, int cmd, int val);
 
 /**
-  * Move the file position to a given offset from a given location
+  * Move the file position to a given offset from a given location.
   *
-  * @param[in]  fd      File handle
-  * @param[in]  offset  The offset from whence to move to
-  * @param[in]  whence  The start of where to seek
-  *      SEEK_SET to start from beginning of file
-  *      SEEK_CUR to start from current position in file
-  *      SEEK_END to start from end of file
+  * @param[in]  fd      the file descriptor of the file.
+  * @param[in]  offset  The offset from whence to move to.
+  * @param[in]  whence  The start of where to seek.
+  *      SEEK_SET to start from beginning of file.
+  *      SEEK_CUR to start from current position in file.
+  *      SEEK_END to start from end of file.
   *
-  * @return  The new offset of the file
+  * @return  The new offset of the file.
   */
 off_t aos_lseek(int fd, off_t offset, int whence);
 
 /**
-  * Flush any buffers associated with the file
+  * Flush any buffers associated with the file.
   *
-  * @param[in]  fd  File handle
+  * @param[in]  fd  the file descriptor of the file.
   *
-  * @return 0 on success, negative error code on failure
+  * @return 0 on success, negative error code on failure.
   */
 int aos_sync(int fd);
 
 /**
-  * Store information about the file in a stat structure
+  * Store information about the file in a stat structure.
   *
-  * @param[in]   path  The name of the file to find information about
-  * @param[out]  st    The stat buffer to write to
+  * @param[in]   path  The path of the file to find information about.
+  * @param[out]  st    The stat buffer to write to.
   *
-  * @return 0 on success, negative error code on failure
+  * @return 0 on success, negative error code on failure.
   */
 int aos_stat(const char *path, struct stat *st);
 
 /**
-  * Remove a file from the filesystem
+  * Remove a file from the filesystem.
   *
-  * @param[in]  path  The name of the file to remove
+  * @param[in]  path  The path of the file to remove.
   *
-  * @return  0 on success, negative error code on failure
+  * @return  0 on success, negative error code on failure.
   */
 int aos_unlink(const char *path);
 
 /**
-  * Rename a file in the filesystem
+  * Rename a file in the filesystem.
   *
-  * @param[in]  oldpath  The name of the file to rename.
-  * @param[in]  newpath  The name to rename it to
+  * @param[in]  oldpath  The path of the file to rename.
+  * @param[in]  newpath  The path to rename it to.
   *
-  * @retval 0 on success, negative error code on failure
+  * @retval 0 on success, negative error code on failure.
   */
 int aos_rename(const char *oldpath, const char *newpath);
 
 /**
-  * Open a directory on the filesystem
+  * Open a directory on the filesystem.
   *
-  * @param[in]  path  Name of the directory to open
+  * @param[in]  path  the path of the directory to open.
   *
-  * @return  a point of directory stream on success, NULL on failure
+  * @return  a point of directory stream on success, NULL on failure.
   */
 aos_dir_t *aos_opendir(const char *path);
 
 /**
-  * Close a directory
+  * Close a directory.
   *
-  * @param[in]  dir  Dir handle
+  * @param[in]  dir  the handle of the directory to close.
   *
-  * @return  0 on success, negative error code on failure
+  * @return  0 on success, negative error code on failure.
   */
 int aos_closedir(aos_dir_t *dir);
 
 /**
-  * Read the next directory entry
+  * Read the next directory entry.
   *
-  * @param[in]  dir  Dir handle
+  * @param[in]  dir  the handle of the directory to read.
   *
-  * @return  a pointer to a dirent structure
+  * @return  a pointer to a dirent structure.
   */
 aos_dirent_t *aos_readdir(aos_dir_t *dir);
 
 /**
-  * Create the directory, if they do not already exist
+  * Create the directory, if they do not already exist.
   *
-  * @param[in]  path  the name of the directory
+  * @param[in]  path  the path of the directory.
   *
-  * @return  0 on success, negative error code on failure
+  * @return  0 on success, negative error code on failure.
   */
 int aos_mkdir(const char *path);
 
