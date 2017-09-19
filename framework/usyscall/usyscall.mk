0cd8952194674e232f935ecbfc11a1a19a603504
Junjie Cai <junjiec@gmail.com>
2017-09-19 20:37:27 +0800

    [syscall] dont add net components

--- framework/usyscall/usyscall.mk
+++ framework/usyscall/usyscall.mk
@@ -1,12 +1,8 @@
 NAME := usyscall
 
-$(NAME)_INCLUDES := ./ \
-                    ../../kernel/protocols/net/include/ \
-                    ../../kernel/protocols/mesh/include/
+$(NAME)_INCLUDES := ./
 
 $(NAME)_CFLAGS += -Wall -Werror
-
-$(NAME)_COMPONENTS := protocols.net
 
 $(NAME)_SOURCES := syscall_uapi.c
 
--- kernel/syscall/syscall.mk
+++ kernel/syscall/syscall.mk
@@ -1,13 +1,9 @@
 NAME := syscall
 
 $(NAME)_TYPE := kernel
-$(NAME)_INCLUDES := ./ \
-                    ../protocols/net/include/ \
-                    ../protocols/mesh/include/
+$(NAME)_INCLUDES := .
 
 $(NAME)_CFLAGS += -Wall -Werror
-
-$(NAME)_COMPONENTS += protocols.net protocols.mesh
 
 $(NAME)_SOURCES := syscall_tbl.c
 
