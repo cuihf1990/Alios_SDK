669055edfeb4c47fb83f41a05ae94723d8371e3c
lingjun chen <lingjun.clj@alibaba.com>
2017-09-20 01:07:16 -0700

    add new hal api hal_flash_erase_write

--- include/hal/soc/flash.h
+++ include/hal/soc/flash.h
@@ -74,7 +74,7 @@ hal_logic_partition_t *hal_flash_get_info(hal_partition_t in_partition);
 int32_t hal_flash_erase(hal_partition_t in_partition, uint32_t off_set, uint32_t size);
 
 /**
- * Write data to an area on a Flash logical partition
+ * Write data to an area on a flash logical partition without erase
  *
  * @param[in]  in_partition    The target flash logical partition which should be read which should be written
  * @param[in]  off_set         Point to the start address that the data is written to, and
@@ -87,7 +87,23 @@ int32_t hal_flash_erase(hal_partition_t in_partition, uint32_t off_set, uint32_t
  * @return  0 : On success, EIO : If an error occurred with any step
  */
 int32_t hal_flash_write(hal_partition_t in_partition, uint32_t *off_set,
-                               const void *in_buf , uint32_t in_buf_len);
+                               const void *in_buf, uint32_t in_buf_len);
+
+/**
+ * Write data to an area on a flash logical partition with erase first
+ *
+ * @param[in]  in_partition    The target flash logical partition which should be read which should be written
+ * @param[in]  off_set         Point to the start address that the data is written to, and
+ *                             point to the last unwritten address after this function is
+ *                             returned, so you can call this function serval times without
+ *                             update this start address.
+ * @param[in]  inBuffer        point to the data buffer that will be written to flash
+ * @param[in]  inBufferLength  The length of the buffer
+ *
+ * @return  0 : On success, EIO : If an error occurred with any step
+ */
+int32_t hal_flash_erase_write(hal_partition_t in_partition, uint32_t *off_set,
+                                        const void *in_buf, uint32_t in_buf_len);
 
 /**
  * Read data from an area on a Flash to data buffer in RAM
