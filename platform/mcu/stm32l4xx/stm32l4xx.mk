818c2591afb63b74293ecf6dd7a8b7c3a32c55e7
woksd <keepwalking.zeng@gmail.com>
2017-09-26 09:19:49 +0100

    fix helloworld@ble

--- platform/mcu/stm32l4xx/stm32l4xx.mk
+++ platform/mcu/stm32l4xx/stm32l4xx.mk
@@ -14,7 +14,7 @@ HOST_OPENOCD := stm32f4x
 $(NAME)_TYPE := kernel
 
 $(NAME)_COMPONENTS += platform/arch/arm/armv7m
-$(NAME)_COMPONENTS := rhino hal netmgr framework.common mbedtls cjson cli
+$(NAME)_COMPONENTS := rhino hal netmgr framework.common mbedtls cjson cli digest_algorithm
 
 GLOBAL_DEFINES += CONFIG_AOS_KV_MULTIPTN_MODE
 GLOBAL_DEFINES += CONFIG_AOS_KV_PTN=6
