159488018f07c1470500a70fbba277261f730775
zhangtian <dongren.zt@alibaba-inc.com>
2017-09-27 14:45:08 +0800

    rename coaptest to coapapp

--- example/coapapp/coapapp.mk
+++ example/coapapp/coapapp.mk
@@ -6,9 +6,9 @@ CONFIG_SYSINFO_APP_VERSION = APP-1.0.0-$(CURRENT_TIME)
 $(info app_version:${CONFIG_SYSINFO_APP_VERSION})
 	GLOBAL_CFLAGS += -DSYSINFO_APP_VERSION=\"$(CONFIG_SYSINFO_APP_VERSION)\"
 
-NAME := coaptest
+NAME := coapapp
 
-$(NAME)_SOURCES     := coaptest.c
+$(NAME)_SOURCES     := coapapp.c
 GLOBAL_DEFINES      += ALIOT_DEBUG
 
 CONFIG_COAP_DTLS_SUPPORT := y
@@ -37,4 +37,4 @@ $(NAME)_CFLAGS += \
     -Wno-unused-function \
 #    -Werror
 
-$(NAME)_COMPONENTS  += connectivity.coap fota
+$(NAME)_COMPONENTS  += connectivity.coap fota netmgr
