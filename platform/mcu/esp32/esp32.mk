67e69a721d3932921b663a9a571334f699e01667
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-10-16 18:32:39 +0800

    [esp32] first commit, scratch

---
+++ platform/mcu/esp32/esp32.mk
@@ -0,0 +1,20 @@
+HOST_OPENOCD := esp32
+
+NAME := esp32
+
+$(NAME)_COMPONENTS += rhino protocols.net
+
+GLOBAL_INCLUDES  += system/include ../../arch/xtensa
+GLOBAL_CFLAGS    += -ffunction-sections -fdata-sections -fstrict-volatile-bitfields -mlongcalls
+GLOBAL_LDFLAGS   += -nostdlib
+
+GLOBAL_LDS_FILES += platform/mcu/esp32/system/ld/esp32.ld.S
+GLOBAL_LDS_FILES += platform/mcu/esp32/system/ld/esp32.common.ld
+GLOBAL_LDS_FILES += platform/mcu/esp32/system/ld/esp32.rom.ld
+GLOBAL_LDS_FILES += platform/mcu/esp32/system/ld/esp32.peripherals.ld
+GLOBAL_LDS_FILES += platform/mcu/esp32/system/ld/esp32.rom.spiram_incompatible_fns.ld
+GLOBAL_LDFLAGS   += -L platform/mcu/esp32/system/ld
+
+$(NAME)_SOURCES  := system/cpu_start.c
+$(NAME)_INCLUDES :=
+$(NAME)_CFLAGS   := -std=gnu99
---
+++ platform/mcu/esp32/system/cpu_start.c
@@ -0,0 +1,6 @@
+#include "esp_attr.h"
+
+void IRAM_ATTR call_start_cpu0()
+{
+}
+
