151b12a6e605ad9eb5d110b427dff879aa997711
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-10-18 19:33:34 +0800

    [mesh] delete files.mk

--- kernel/protocols/mesh/mesh.mk
+++ kernel/protocols/mesh/mesh.mk
@@ -7,8 +7,30 @@ $(NAME)_INCLUDES += include
 
 $(NAME)_COMPONENTS += yloop
 
-include kernel/protocols/mesh/files.mk
-$(NAME)_SOURCES := $(umesh_srcs)
+$(NAME)_SOURCES := src/core/umesh.c \
+                   src/core/mesh/mesh_mgmt.c \
+                   src/core/mesh/network_mgmt.c \
+                   src/core/mesh/link_mgmt.c \
+                   src/core/mesh/network_data.c \
+                   src/core/mesh/mesh_forwarder.c \
+                   src/core/mesh/address_mgmt.c \
+                   src/core/mesh/lowpan6.c \
+                   src/core/mesh/mcast.c \
+                   src/core/routing/router_mgr.c \
+                   src/core/routing/sid_router.c \
+                   src/core/routing/ssid_allocator.c \
+                   src/core/security/keys_mgr.c \
+                   src/core/security/crypto.c \
+                   src/ip/ip6_address.c \
+                   src/hal/interfaces.c \
+                   src/hal/umesh_hal.c \
+                   src/hal/80211.c \
+                   src/utilities/message.c \
+                   src/utilities/timer.c \
+                   src/utilities/memory.c \
+                   src/utilities/configs.c \
+                   src/utilities/task.c \
+                   src/tools/cli.c
 
 MESHDEBUG ?= 1
 ifeq ($(MESHDEBUG), 1)
