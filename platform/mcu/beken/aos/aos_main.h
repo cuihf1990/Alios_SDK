3208f211ad7b175dbcebda2040679bdd119ac6a7
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2017-10-18 14:37:25 +0800

    [BINS]: cloud from framework to kernel

--- platform/mcu/beken/aos/aos_main.h
+++ platform/mcu/beken/aos/aos_main.h
@@ -11,16 +11,5 @@ void soc_system_init(void);
 
 void sys_start(void);
 
-struct app_info_t {
-     int (*app_entry)(void *syscall_tbl, int argc, char *argv[]);
-     unsigned int data_ram_start;
-     unsigned int data_ram_end;
-     unsigned int data_flash_begin;
-     unsigned int bss_start;
-     unsigned int bss_end;
-     unsigned int heap_start;
-     unsigned int heap_end;
-};
-
 #endif /* AOS_MAIN_H */
 
