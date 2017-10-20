88fc7e548ce730cb66f3aae39475c1878b866e25
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-10-20 09:21:56 +0800

    [mesh] move aes to platform

--- kernel/protocols/mesh/src/platform/aos.c
+++ kernel/protocols/mesh/src/platform/aos.c
@@ -4,7 +4,11 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+
 #include <aos/aos.h>
+#include <ali_crypto.h>
+
+#include <umesh_types.h>
 
 /*
  * symbols to export
@@ -53,4 +57,107 @@ void umesh_pal_log(const char *fmt, ...)
     vprintf(fmt, args);
     va_end(args);
     printf("\r\n");
+}
+
+/*
+ * security
+ */
+typedef void *umesh_aes_ctx_t;
+
+uint8_t g_umesh_iv[] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+static ur_error_t umesh_aes_encrypt_decrypt(umesh_aes_ctx_t *aes,
+                                            const void *src,
+                                            uint16_t size,
+                                            void *dst)
+{
+    ali_crypto_result result;
+    uint32_t dlen = 1024;
+
+    if (aes == NULL) {
+        return UR_ERROR_FAIL;
+    }
+
+    result = ali_aes_finish(src, size, dst, &dlen, SYM_NOPAD, aes);
+
+    if (result != ALI_CRYPTO_SUCCESS) {
+        return UR_ERROR_FAIL;
+    }
+
+    return UR_ERROR_NONE;
+}
+
+ur_error_t umesh_pal_aes_encrypt(const uint8_t *key, uint8_t key_size,
+                             const void *src,
+                             uint16_t size, void *dst)
+{
+    ur_error_t error;
+    umesh_aes_ctx_t *aes;
+    uint32_t aes_ctx_size;
+    ali_crypto_result result;
+
+    if (key == NULL || src == NULL || dst == NULL) {
+        return UR_ERROR_FAIL;
+    }
+
+    result = ali_aes_get_ctx_size(AES_CTR, &aes_ctx_size);
+    if (result != ALI_CRYPTO_SUCCESS) {
+        return UR_ERROR_FAIL;
+    }
+
+    aes = aos_malloc(aes_ctx_size);
+    if (aes == NULL) {
+        return UR_ERROR_FAIL;
+    }
+
+    result = ali_aes_init(AES_CTR, true,
+                          key, NULL, key_size, g_umesh_iv, aes);
+    if (result != ALI_CRYPTO_SUCCESS) {
+        aos_free(aes);
+        return UR_ERROR_FAIL;
+    }
+
+    error = umesh_aes_encrypt_decrypt(aes, src, size, dst);
+    aos_free(aes);
+
+    return error;
+}
+
+ur_error_t umesh_pal_aes_decrypt(const uint8_t *key, uint8_t key_size,
+                             const void *src,
+                             uint16_t size, void *dst)
+{
+    ur_error_t error;
+    umesh_aes_ctx_t *aes;
+    uint32_t aes_ctx_size;
+    ali_crypto_result result;
+
+    if (key == NULL || src == NULL || dst == NULL) {
+        return UR_ERROR_FAIL;
+    }
+
+    result = ali_aes_get_ctx_size(AES_CTR, &aes_ctx_size);
+    if (result != ALI_CRYPTO_SUCCESS) {
+        return UR_ERROR_FAIL;
+    }
+
+    aes = aos_malloc(aes_ctx_size);
+    if (aes == NULL) {
+        return UR_ERROR_FAIL;
+    }
+
+    result = ali_aes_init(AES_CTR, false,
+                          key, NULL, key_size, g_umesh_iv, aes);
+    if (result != ALI_CRYPTO_SUCCESS) {
+        aos_free(aes);
+        return UR_ERROR_FAIL;
+    }
+
+    error = umesh_aes_encrypt_decrypt(aes, src, size, dst);
+    aos_free(aes);
+
+    return error;
 }
