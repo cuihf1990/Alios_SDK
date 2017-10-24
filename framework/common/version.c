a130967dec6021d745ce900e3a54657ba75a8a15
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-10-24 11:28:41 +0800

    [framework] save verion only if kv is enabled

--- framework/common/version.c
+++ framework/common/version.c
@@ -71,7 +71,9 @@ void version_init(void)
 {
     aos_cli_register_command(&versioncmd);
     const char *app_version = aos_get_app_version();
+#ifdef AOS_KV
     aos_kv_set(KEY_APP_VER, app_version, strlen(app_version),1);
+#endif
 }
 
 AOS_EXPORT AOS_WEAK void dump_sys_info(void)
