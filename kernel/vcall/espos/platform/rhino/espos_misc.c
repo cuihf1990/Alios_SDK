359ee124067490f950fb1878a8688e24eeedda41
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-10-27 15:05:07 +0800

    [vcall] added espos

---
+++ kernel/vcall/espos/platform/rhino/espos_misc.c
@@ -0,0 +1,13 @@
+#include "k_api.h"
+#include "espos_err.h"
+
+int espos_err_map (kstat_t err_code)
+{
+    if (err_code == RHINO_SUCCESS) {
+        return 0;
+    }
+
+    return -1;
+}
+
+
--- kernel/vcall/vcall.mk
+++ kernel/vcall/vcall.mk
@@ -8,26 +8,34 @@ ifeq ($(HOST_ARCH),ARM968E-S)
 $(NAME)_CFLAGS += -marm
 endif
 
+vcall ?= rhino
+
 ifeq ($(vcall),freertos)
 GLOBAL_DEFINES += VCALL_FREERTOS
 
 $(NAME)_SOURCES += \
     aos/aos_freertos.c
-else
-ifneq ($(vcall),posix)
-GLOBAL_DEFINES += VCALL_RHINO
-$(NAME)_COMPONENTS += rhino
+endif
 
-$(NAME)_SOURCES := \
-    mico/mico_rhino.c
-
-$(NAME)_SOURCES += \
-    aos/aos_rhino.c
-else
+ifeq ($(vcall),posix)
 GLOBAL_DEFINES += VCALL_POSIX
 
 $(NAME)_SOURCES += \
     aos/aos_posix.c
 endif
+
+ifeq ($(vcall),rhino)
+GLOBAL_DEFINES += VCALL_RHINO
+$(NAME)_COMPONENTS += rhino
+
+ifneq ($(HOST_MCU_FAMILY),esp32)
+$(NAME)_SOURCES := \
+    mico/mico_rhino.c
+else
+$(NAME)_COMPONENTS += vcall.espos
+endif
+
+$(NAME)_SOURCES += \
+    aos/aos_rhino.c
 endif
 
