b3bfaa60fe9cf8537f94ab7ffa8473906a9d4db3
Wenjun Chen <wenjunchen.cwj@alibaba.com>
2017-10-30 21:26:29 +0800

    [esp32 netmgr] Add netmgrapp that includes netmgr but without awss/alink.

--- platform/mcu/esp32/bsp/entry.c
+++ platform/mcu/esp32/bsp/entry.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 #include <hal/soc/uart.h>
+#include <hal/wifi.h>
 #include <aos/aos.h>
 
 #include <sdkconfig.h>
@@ -22,78 +23,19 @@ static void app_entry(void *arg)
 }
 
 #ifdef ENABLE_WIFI
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_event_loop.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
 #include "lwip/sockets.h"
-
 int __attribute__((weak)) eventfd(unsigned int initval, int flags)
 {
     return socket(AF_INET, SOCK_DGRAM, 0);
 }
-static const char *TAG = "example";
-/* The examples use simple WiFi configuration that you can set via
-   'make menuconfig'.
 
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_WIFI_SSID "alibaba-guest"
-#define EXAMPLE_WIFI_PASS "sh@alibaba"
-
-/* FreeRTOS event group to signal when we are connected & ready to make a request */
-static EventGroupHandle_t wifi_event_group;
-
-/* The event group allows multiple bits for each event,
-   but we only care about one event - are we connected
-   to the AP with an IP? */
-const int CONNECTED_BIT = BIT0;
-
-static esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-        esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-        /* This is a workaround as ESP32 WiFi libs don't currently
-           auto-reassociate. */
-        esp_wifi_connect();
-        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    default:
-        break;
-    }
-    return ESP_OK;
-}
-
+extern void tcpip_adapter_init(void);
+extern hal_wifi_module_t sim_aos_wifi_eps32;
 static void initialise_wifi(void)
 {
     tcpip_adapter_init();
-    wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_WIFI_SSID,
-            .password = EXAMPLE_WIFI_PASS,
-        },
-    };
-    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
+    hal_wifi_register_module(&sim_aos_wifi_eps32);
+    hal_wifi_init();
 }
 #else
 static void initialise_wifi(void)
