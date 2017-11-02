b8d6842607c142284ef9dfba1aa7ed70a0335781
woksd <keepwalking.zeng@gmail.com>
2017-11-02 10:29:49 +0000

    add header file for keil c lib

---
+++ utility/libc/compilers/armlibc/sys/types.h
@@ -0,0 +1,11 @@
+#ifndef _SYS_TYPES_H
+#define _SYS_TYPES_H
+
+#include <stdint.h>
+
+typedef uint32_t clockid_t;
+typedef uint32_t key_t;         /* Used for interprocess communication. */
+typedef uint32_t pid_t;         /* Used for process IDs and process group IDs. */
+typedef signed long ssize_t;    /* Used for a count of bytes or an error indication. */
+
+#endif
