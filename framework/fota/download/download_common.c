0b32936e78386e0a5cd901550ac316c787af52d0
zhangtian <dongren.zt@alibaba-inc.com>
2017-11-08 18:04:25 +0800

    [fota] breakpoint resume configurable

--- framework/fota/download/download_common.c
+++ framework/fota/download/download_common.c
@@ -20,48 +20,65 @@
 
 void save_state(uint32_t breakpoint, MD5_CTX *pMD5)
 {
+#ifdef CONFIG_AOS_FOTA_BREAKPOINT     
     ota_set_update_breakpoint(breakpoint);
     ota_set_cur_MD5_context(pMD5);
+#endif     
 }
 
 uint32_t ota_get_update_breakpoint()
 {
     uint32_t offset = 0;
+#ifdef CONFIG_AOS_FOTA_BREAKPOINT    
     int len = 4;
-
     if (aos_kv_get(KEY_OTA_BREAKPOINT, &offset, &len)) {
         offset = 0;
     }
+#endif    
     //OTA_LOG_I("ota_get_update_breakpoint=%d",offset);
     return offset;
 }
 
 int ota_set_update_breakpoint(uint32_t offset)
 {
-    //OTA_LOG_I("ota_set_update_breakpoint=%d",offset);
+#ifdef CONFIG_AOS_FOTA_BREAKPOINT   
     return  aos_kv_set(KEY_OTA_BREAKPOINT, &offset, 4, 1);
+#endif
+    return 0;    
 }
 
 int ota_get_last_MD5(char *value)
 {
+#ifdef CONFIG_AOS_FOTA_BREAKPOINT       
     int len = 33;
     int ret = aos_kv_get(KEY_OTA_MD5, value, &len);
     return ret;
+#endif
+    return 0;    
 }
 
 int ota_set_cur_MD5(char *value)
 {
+#ifdef CONFIG_AOS_FOTA_BREAKPOINT     
     return  aos_kv_set(KEY_OTA_MD5, value, 33, 1);
+#endif
+    return 0;    
 }
 
 int ota_get_last_MD5_context(MD5_CTX *md5ctx)
 {
+#ifdef CONFIG_AOS_FOTA_BREAKPOINT      
     int len = sizeof(MD5_CTX);
-    int ret = aos_kv_get(KEY_OTA_MD5_CTX, md5ctx, &len);
-    return ret;
+    return aos_kv_get(KEY_OTA_MD5_CTX, md5ctx, &len);
+#endif
+    return 0; 
+    
 }
 
 int ota_set_cur_MD5_context(MD5_CTX *md5ctx)
 {
+#ifdef CONFIG_AOS_FOTA_BREAKPOINT     
     return  aos_kv_set(KEY_OTA_MD5_CTX, md5ctx, sizeof(MD5_CTX), 1);
+#endif 
+    return 0;    
 }
