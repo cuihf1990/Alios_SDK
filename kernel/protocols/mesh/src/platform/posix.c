cd5124e2bca01c2bf90e4a47f86975968553de25
luwang <wanglu.luwang@alibaba-inc.com>
2017-11-09 18:45:59 +0800

    Fix for coding style

--- kernel/protocols/mesh/src/platform/posix.c
+++ kernel/protocols/mesh/src/platform/posix.c
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
--- utility/libc/compilers/armlibc/sys/select.h
+++ utility/libc/compilers/armlibc/sys/select.h
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
 #ifndef _SYS_SELECT_H__
 #define _SYS_SELECT_H__
 
