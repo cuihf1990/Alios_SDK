020f33bf8490f96d14186f6ffc7a47f7a60ef559
Junjie Cai <simen.cjj@alibaba-inc.com>
2017-11-09 09:39:30 +0800

    [cli] lld->ld to avoid newlibc poor lld support

--- tools/cli/cli.c
+++ tools/cli/cli.c
@@ -727,7 +727,7 @@ static void reboot_cmd(char *buf, int len, int argc, char **argv)
 
 static void uptime_cmd(char *buf, int len, int argc, char **argv)
 {
-    aos_cli_printf("UP time %lldms\r\n", aos_now_ms());
+    aos_cli_printf("UP time %ldms\r\n", (long)aos_now_ms());
 }
 
 void tftp_ota_thread(void *arg)
