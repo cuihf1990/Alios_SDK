e116cd8323313c665569fe2995cdca04c9869b81
杨海波 <yanghb@mxchip.com>
2017-11-30 13:50:41 +0800

    set HAL_PARTITION_PARAMETER_2 size to 8KB. kv partition should not less than 8K.

--- board/mk3166/board.c
+++ board/mk3166/board.c
@@ -28,30 +28,30 @@ void board_init(void)
     hal_partitions[HAL_PARTITION_PARAMETER_1].partition_owner            = HAL_FLASH_QSPI;
     hal_partitions[HAL_PARTITION_PARAMETER_1].partition_description      = "PARAMETER1";
     hal_partitions[HAL_PARTITION_PARAMETER_1].partition_start_addr       = 0x0;
-    hal_partitions[HAL_PARTITION_PARAMETER_1].partition_length           = 0x1000; // 4k bytes
+    hal_partitions[HAL_PARTITION_PARAMETER_1].partition_length           = 0x2000; // 8k bytes
     hal_partitions[HAL_PARTITION_PARAMETER_1].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
 
     hal_partitions[HAL_PARTITION_PARAMETER_2].partition_owner            = HAL_FLASH_QSPI;
     hal_partitions[HAL_PARTITION_PARAMETER_2].partition_description      = "PARAMETER2";
-    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_start_addr       = 0x1000;
-    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_length           = 0x1000; //4k bytes
+    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_start_addr       = 0xDA000;
+    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_length           = 0x2000; //8k bytes
     hal_partitions[HAL_PARTITION_PARAMETER_2].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
 
     hal_partitions[HAL_PARTITION_OTA_TEMP].partition_owner               = HAL_FLASH_EMBEDDED;
     hal_partitions[HAL_PARTITION_OTA_TEMP].partition_description         = "OTA Storage";
-    hal_partitions[HAL_PARTITION_OTA_TEMP].partition_start_addr          = 0x42000;
+    hal_partitions[HAL_PARTITION_OTA_TEMP].partition_start_addr          = 0x40000;
     hal_partitions[HAL_PARTITION_OTA_TEMP].partition_length              = 0x98000;    //608K bytes
     hal_partitions[HAL_PARTITION_OTA_TEMP].partition_options             = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
 
     hal_partitions[HAL_PARTITION_PARAMETER_3].partition_owner            = HAL_FLASH_QSPI;
     hal_partitions[HAL_PARTITION_PARAMETER_3].partition_description      = "PARAMETER3";
-    hal_partitions[HAL_PARTITION_PARAMETER_3].partition_start_addr       = 0x40000;
+    hal_partitions[HAL_PARTITION_PARAMETER_3].partition_start_addr       = 0xD8000;
     hal_partitions[HAL_PARTITION_PARAMETER_3].partition_length           = 0x1000; //4k bytes
     hal_partitions[HAL_PARTITION_PARAMETER_3].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
 
     hal_partitions[HAL_PARTITION_PARAMETER_4].partition_owner            = HAL_FLASH_QSPI;
     hal_partitions[HAL_PARTITION_PARAMETER_4].partition_description      = "PARAMETER4";
-    hal_partitions[HAL_PARTITION_PARAMETER_4].partition_start_addr       = 0x41000;
+    hal_partitions[HAL_PARTITION_PARAMETER_4].partition_start_addr       = 0xD9000;
     hal_partitions[HAL_PARTITION_PARAMETER_4].partition_length           = 0x1000; //4k bytes
     hal_partitions[HAL_PARTITION_PARAMETER_4].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
 }
