6085b803a21611fd207a6defaf5922a4fd10bbfa
Xiao Ma <xiaoma.mx@alibaba-inc.com>
2017-12-06 16:02:39 +0800

    [build] update armhflinux build fix per comments.

--- kernel/protocols/mesh/src/ip/lwip_adapter.c
+++ kernel/protocols/mesh/src/ip/lwip_adapter.c
@@ -18,7 +18,6 @@
 
 #ifdef CONFIG_AOS_MESH_TAPIF
 static bool is_router;
-static bool is_goto_gateway;
 int umesh_tapif_init(const char *ifname);
 void umesh_tapif_deinit(void);
 void umesh_tapif_send_pbuf(struct pbuf *pbuf);
@@ -108,7 +107,7 @@ ur_error_t ur_adapter_resolve_ip(const void *ipaddr, ur_addr_t *addr)
                ip4_addr_cmp(ip4_addr, netif_ip4_gw(netif))) {
 #ifdef CONFIG_AOS_MESH_TAPIF
         if (is_router) {
-            is_goto_gateway = true;
+            MESH_LOG_DEBUG("should go to gateway\n");
             return UR_ERROR_FAIL;
         }
 #endif
@@ -140,17 +139,14 @@ static err_t ur_adapter_ipv4_output(struct netif *netif, struct pbuf *p,
 
     error = ur_adapter_resolve_ip(dest, &addr);
 
-#ifdef CONFIG_AOS_MESH_TAPIF
-    if (error == UR_ERROR_FAIL && is_goto_gateway) {
-        MESH_LOG_DEBUG("should go to gateway\n");
-        umesh_tapif_send_pbuf(p);
-        is_goto_gateway = false;
-    }
-#endif
-
     if (error == UR_ERROR_NONE) {
         error = umesh_output_sid(p, addr.netid, addr.addr.short_addr);
     }
+#ifdef CONFIG_AOS_MESH_TAPIF
+    else if (is_router) {
+        umesh_tapif_send_pbuf(p);
+    }
+#endif
 
     return err_mapping(error);
 }
