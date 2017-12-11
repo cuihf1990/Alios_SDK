b9f7cc13f5342f0cb3322d81f096a7ae862259e4
minghao.zmh <minghao.zmh@alibaba-inc.com>
2017-12-10 23:02:45 -0800

    Add mqtt test code for stm32l475e.

--- example/mqttest/mqttest.mk
+++ example/mqttest/mqttest.mk
@@ -1,42 +1,22 @@
-NAME := mqttapp
+ifneq (,${BINS})
+	GLOBAL_CFLAGS += -DSYSINFO_OS_BINS
+endif
+CURRENT_TIME = $(shell /bin/date +%Y%m%d.%H%M)
+CONFIG_SYSINFO_APP_VERSION = APP-1.0.0-$(CURRENT_TIME)
+$(info app_version:${CONFIG_SYSINFO_APP_VERSION})
+	GLOBAL_CFLAGS += -DSYSINFO_APP_VERSION=\"$(CONFIG_SYSINFO_APP_VERSION)\"
+
+NAME := mqttest
 
 GLOBAL_DEFINES      += ALIOT_DEBUG IOTX_DEBUG
 CONFIG_OTA_CH = mqtt
-
-
+ifeq ($(SPI_WIFI_ENABLED), true)
+$(NAME)_SOURCES     := mqttest-b_l475e.c
+else
 $(NAME)_SOURCES     := mqttest.c
-
-ifeq ($(findstring linuxhost, $(BUILD_STRING)), linuxhost)
-PLATFORM_MQTT := linux
-#NETWORK_MQTT := linuxsock
-else ifeq ($(findstring linuxhost, $(BUILD_STRING)), linuxhost)
-PLATFORM_MQTT := linux
-#NETWORK_MQTT := linuxsock
-else ifeq ($(findstring mk3060, $(BUILD_STRING)), mk3060)
-PLATFORM_MQTT := rhino
-#NETWORK_MQTT := rhinosock
 endif
 
-$(NAME)_SOURCES += ../../utility/iotx-utils/hal/$(PLATFORM_MQTT)/HAL_OS_$(PLATFORM_MQTT).c
-$(NAME)_SOURCES += ../../utility/iotx-utils/hal/$(PLATFORM_MQTT)/HAL_TCP_$(PLATFORM_MQTT).c
-
-$(NAME)_SOURCES += ../../utility/iotx-utils/mbedtls-lib/HAL_TLS_mbedtls.c
-
-#$(NAME)_INCLUDES := ../../framework/protocol/alink/json/
-
-$(NAME)_INCLUDES  += ../../utility/iotx-utils/hal \
-../../utility/iotx-utils/digest \
-../../utility/iotx-utils/mqtt \
-../../utility/iotx-utils/guider \
-../../utility/iotx-utils/LITE-log \
-../../utility/iotx-utils/LITE-utils \
-../../utility/iotx-utils/sdk-impl \
-../../utility/iotx-utils/sdk-impl/imports \
-../../utility/iotx-utils/misc
-#$(NAME)_COMPONENTS := log connectivity protocol.alink.json
-$(NAME)_COMPONENTS := log
-$(NAME)_COMPONENTS  += mbedtls
-$(NAME)_COMPONENTS  += connectivity.mqtt
+$(NAME)_COMPONENTS := cli connectivity.mqtt cjson fota netmgr framework.common
 
 LWIP := 0
 ifeq ($(LWIP),1)
