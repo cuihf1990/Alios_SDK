6a498733522c2ff2d5e7d4ec1c952f54b39badc7
woksd <keepwalking.zeng@gmail.com>
2017-12-12 23:48:36 +0000

    support armcc libc

--- utility/libc/libc.mk
+++ utility/libc/libc.mk
@@ -1,6 +1,16 @@
 NAME := newlib_stub
 
+ifeq ($(COMPILER),armcc)
+GLOBAL_INCLUDES += compilers/armlibc 
+else 
+ifeq ($(COMPILER),EWARM)
+GLOBAL_INCLUDES += compilers/EWARM 
+else
+#default gcc 
 ifneq ($(HOST_MCU_FAMILY),linux)
 $(NAME)_TYPE := share
 $(NAME)_SOURCES := newlib_stub.c
 endif
+endif
+endif
+
