da231aa9efd61d9601f0f08d6287c9d86e5c1689
woksd <keepwalking.zeng@gmail.com>
2017-12-12 11:08:57 +0000

    modify kernel&framework mk for multi compiler

--- framework/common/common.mk
+++ framework/common/common.mk
@@ -8,6 +8,7 @@ GLOBAL_DEFINES += AOS_FRAMEWORK_COMMON
 ifneq (,${BINS})
         GLOBAL_CFLAGS += -DSYSINFO_OS_BINS
 endif
+
 CURRENT_TIME = $(shell /bin/date +%Y%m%d.%H%M)
 CONFIG_SYSINFO_APP_VERSION = app-1.0.0-$(CURRENT_TIME)
 $(info app_version:${CONFIG_SYSINFO_APP_VERSION})
