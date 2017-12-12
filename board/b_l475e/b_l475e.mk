6826fe117313b475f15394780f0659201e33da8a
Fan Qian <pigmike1018@126.com>
2017-12-12 20:23:31 +0800

    [yts] add cli support for board yts, add default testcase set for b_l475e

--- board/b_l475e/b_l475e.mk
+++ board/b_l475e/b_l475e.mk
@@ -38,3 +38,9 @@ FILESYSTEM_IMAGE_SECTOR_START := 256    #0x100000
 # Extra build target in mico_standard_targets.mk, include bootloader, and copy output file to eclipse debug file (copy_output_for_eclipse)
 EXTRA_TARGET_MAKEFILES +=  $(MAKEFILES_PATH)/aos_standard_targets.mk
 #EXTRA_TARGET_MAKEFILES +=  $(SOURCE_ROOT)/platform/mcu/$(HOST_MCU_FAMILY)/gen_crc_bin.mk
+
+# Define default component testcase set
+ifeq (, $(findstring yts, $(BUILD_STRING)))
+GLOBAL_DEFINES += RHINO_CONFIG_WORKQUEUE=1
+TEST_COMPONENTS += basic api wifi_hal rhino vcall kv yloop alicrypto cjson digest_algorithm hashtable
+endif
