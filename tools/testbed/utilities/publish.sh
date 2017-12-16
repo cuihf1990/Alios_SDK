4d3da5ac6149082c2ed26f046c3ecd88dedaf5f3
Li Cheng <lc122798@alibaba-inc.com>
2017-12-16 23:06:51 +0800

    [testbed] split board specific logic from client.py

--- tools/testbed/utilities/publish.sh
+++ tools/testbed/utilities/publish.sh
@@ -13,16 +13,19 @@ for target in $@; do
     if [ "${target}" = "server" ]; then
         echo -e "publish and restart server\n"
         scp *.py ${server}:/home/lc122798/testbed/
+        scp -r board ${server}:/home/lc122798/testbed/
         ssh ${server} 'tbserver_start'
         sleep 2
     elif [[ ${target} == lab* ]]; then
         echo -e "\npublish and restart client at aos${target}\n"
         scp *.py ${target}:/home/iot/testbed/
+        scp -r board ${target}:/home/iot/testbed/
         ssh ${target} 'tbclient_start'
         sleep 2
     elif [[ ${target} == Pi* ]]; then
         echo -e "\npublish and restart client at ${target}\n"
         scp *.py ${target}:/home/pi/testbed/
+        scp -r board ${target}:/home/pi/testbed/
         ssh ${target} 'tbclient_start'
         sleep 2
     else
