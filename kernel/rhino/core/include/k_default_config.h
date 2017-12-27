0784557c9deebaf7cd2186d148dbf0168f6c8539
lingjun chen <lingjun.clj@alibaba.com>
2017-12-26 18:33:40 -0800

    decrease timer msg size

--- kernel/rhino/core/include/k_default_config.h
+++ kernel/rhino/core/include/k_default_config.h
@@ -179,7 +179,7 @@
 #endif
 
 #ifndef RHINO_CONFIG_TIMER_MSG_NUM
-#define RHINO_CONFIG_TIMER_MSG_NUM           40
+#define RHINO_CONFIG_TIMER_MSG_NUM           20
 #endif
 
 /* kernel intrpt conf */
