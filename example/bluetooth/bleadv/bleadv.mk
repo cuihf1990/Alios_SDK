508925feef9ce36d0a9bd4772b79c151d64f102f
luwang <wanglu.luwang@alibaba-inc.com>
2018-01-05 16:35:09 +0800

    Remove bleadv sample

--- example/bluetooth/bleadv/bleadv.mk
+++
@@ -1,9 +0,0 @@
-NAME := bleadv
-
-$(NAME)_SOURCES     := main.c
-
-$(NAME)_INCLUDES    := ../../../kernel/protocols/bluetooth/include \
-                       ../../../kernel/protocols/bluetooth/core/include
-
-$(NAME)_COMPONENTS  += protocols.bluetooth yloop cli
-GLOBAL_DEFINES      += AOS_NO_WIFI CONFIG_BLUETOOTH_DEBUG
