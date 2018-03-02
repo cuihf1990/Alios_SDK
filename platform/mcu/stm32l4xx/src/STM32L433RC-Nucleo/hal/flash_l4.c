32619f22a49c62a8c8066175c47701c769c18eaa
Fan Qian <pigmike1018@126.com>
2018-03-02 11:24:28 +0800

    fix flash issue

--- platform/mcu/stm32l4xx/src/STM32L433RC-Nucleo/hal/flash_l4.c
+++ platform/mcu/stm32l4xx/src/STM32L433RC-Nucleo/hal/flash_l4.c
@@ -46,8 +46,6 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
-//#include "main.h"
-#include "flash.h"
 #include <string.h>
 #include <stdbool.h>
 #include "stm32l4xx.h"
@@ -61,6 +59,24 @@
 /* Private variables ----------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
 /* Functions Definition ------------------------------------------------------*/
+
+static uint32_t FLASH_GetPage(uint32_t Addr)
+{
+  uint32_t page = 0;
+  
+  if (Addr < (FLASH_BASE + FLASH_BANK_SIZE))
+  {
+    /* Bank 1 */
+    page = (Addr - FLASH_BASE) / FLASH_PAGE_SIZE;
+  }
+  else
+  {
+    /* Bank 2 */
+    page = (Addr - (FLASH_BASE + FLASH_BANK_SIZE)) / FLASH_PAGE_SIZE;
+  }
+  
+  return page;
+}
 
 /**
   * @brief  Erase FLASH memory page(s) at address.
@@ -82,18 +98,10 @@ int FLASH_unlock_erase(uint32_t address, uint32_t len_bytes)
    * After erase, the flash is left in unlocked state.
    */
   EraseInit.TypeErase = FLASH_TYPEERASE_PAGES;
-  
-  EraseInit.Banks = FLASH_get_bank(address); 
-  if (EraseInit.Banks != FLASH_get_bank(address + len_bytes))
-  {
-    printf("Error: Cannot erase across FLASH banks.\n");
-  }
-  else
-  {
-    EraseInit.Page = FLASH_get_pageInBank(address);
-    EraseInit.NbPages = FLASH_get_pageInBank(address + len_bytes - 1) - EraseInit.Page + 1;
+  EraseInit.Page = FLASH_GetPage(address);
+  EraseInit.NbPages = FLASH_GetPage(address + len_bytes - 1) - EraseInit.Page + 1;
     
-    HAL_FLASH_Unlock();
+  HAL_FLASH_Unlock();
     
     if (HAL_FLASHEx_Erase(&EraseInit, &PageError) == HAL_OK)
     {
@@ -103,7 +111,6 @@ int FLASH_unlock_erase(uint32_t address, uint32_t len_bytes)
     {
       printf("Error erasing at 0x%08lx\n", address);
     }
-  }
   return rc;
 }
 
@@ -154,62 +161,6 @@ int FLASH_write_at(uint32_t address, uint64_t *pData, uint32_t len_bytes)
 #endif
   return ret;
 }
-
-/**
-  * @brief  Get the bank of a given address.
-  * @param  In: addr      Address in the FLASH Memory.
-  * @retval Bank identifier.
-  *           FLASH_BANK_1
-  *           FLASH_BANK_2
-  */
-uint32_t FLASH_get_bank(uint32_t addr)
-{
-  uint32_t bank = 0;
-#if 0
-  if (READ_BIT(SYSCFG->MEMRMP, SYSCFG_MEMRMP_FB_MODE) == 0)
-  {
-    /* No Bank swap */
-    bank = (addr < (FLASH_BASE + FLASH_BANK_SIZE)) ? FLASH_BANK_1 : FLASH_BANK_2;
-  }
-  else
-  {
-    /* Bank swap */
-    bank = (addr < (FLASH_BASE + FLASH_BANK_SIZE)) ? FLASH_BANK_2 : FLASH_BANK_1;
-  }
-#endif 
-  
-  bank = FLASH_BANK_1;
-  return bank;
-}
-
-/**
-  * @brief  Get the page of a given address within its FLASH bank.
-  * @param  In: addr    Address in the FLASH memory.
-  * @retval >=0 Success: Page number.
-  *          <0 Failure: The address in not mapped in the internal FLASH memory.
-  */
-int FLASH_get_pageInBank(uint32_t addr)
-{
-  int page = -1;
-
-  if ( ((FLASH_BASE + FLASH_SIZE) > addr) && (addr >= FLASH_BASE) )
-  {
-    /* The address is in internal FLASH range. */
-    if ( addr < (FLASH_BASE + FLASH_BANK_SIZE) )
-    { 
-      /* Addr in the first bank */
-      page = (addr - FLASH_BASE) / FLASH_PAGE_SIZE;
-    }
-    else 
-    {
-      /* Addr in the second bank */
-      page = (addr - FLASH_BASE - FLASH_BANK_SIZE) / FLASH_PAGE_SIZE;
-    }
-  }
-  
-  return page;
-}
-
 
 /**
   * @brief  Update a chunk of the FLASH memory.
@@ -270,68 +221,6 @@ int FLASH_update(uint32_t dst_addr, const void *data, uint32_t size)
   return ret;
 }
 
-/**
- * @brief   Set the FLASH bank from which the bootloader which start after the next reset.
- * @note    See also the FLASH_BualBoot L4 example project.
- * @param   bank: Boot target.
- *            FLASH_BANK_1
- *            FLASH_BANK_2
- *            FLASH_BANK_BOTH   Switch to the other bank than the one which is currently used.
- * @retval  0: Success.
- *         <0: Failure.
- */
-#if 0
-int FLASH_set_boot_bank(uint32_t bank)
-{
-  int rc = 0;
-  FLASH_OBProgramInitTypeDef    OBInit;          
-  /* Set BFB2 bit to enable boot from Flash Bank2 */
-  /* Allow Access to the Flash control registers and user Flash. */
-  HAL_FLASH_Unlock();  
-  /* Clear OPTVERR bit set on virgin samples. */                       
-  __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_OPTVERR); 
-  /* Allow Access to the option bytes sector. */
-  HAL_FLASH_OB_Unlock();  
-  /* Get the Dual boot configuration status. */                    
-  HAL_FLASHEx_OBGetConfig(&OBInit);           
-  
-  /* Enable/Disable dual boot feature */
-  OBInit.OptionType = OPTIONBYTE_USER;
-  OBInit.USERType   = OB_USER_BFB2;
-  switch (bank)
-  {
-    case FLASH_BANK_1:
-      OBInit.USERConfig = OB_BFB2_DISABLE;
-      break;
-    case FLASH_BANK_2:
-      OBInit.USERConfig = OB_BFB2_ENABLE;
-      break;
-    case FLASH_BANK_BOTH:
-      OBInit.USERConfig = ( (OBInit.USERConfig & OB_BFB2_ENABLE) == OB_BFB2_ENABLE ) ? OB_BFB2_DISABLE : OB_BFB2_ENABLE;
-      break;
-    default:
-      rc = -1;
-  }
-  
-  if(HAL_FLASHEx_OBProgram (&OBInit) != HAL_OK)
-  { /* Failed setting the option bytes configuration.
-     * Call 'HAL_FLASH_GetError()' for details. */
-    rc = -1;
-    printf("Error: Could not init the option bytes programming.\n");
-  }
-  else
-  { /* Start the Option Bytes programming process */  
-    if (HAL_FLASH_OB_Launch() != HAL_OK)  
-    { /* Failed reloading the option bytes configuration.
-       * Call 'HAL_FLASH_GetError()' for details. */
-      rc = -1;
-      printf("Error: Could not program the 2nd bank boot option byte.\n");
-    }
-  }
-  
-  return rc;
-}
-#endif 
 /*****************************************************************/
 /**
   * @brief  Read from FLASH memory.
@@ -358,20 +247,5 @@ int FLASH_read_at(uint32_t address, uint64_t *pData, uint32_t len_bytes)
     return ret;
 }
 
-#if 0
-int FLASH_bank1_enabled( )
-{
-    FLASH_OBProgramInitTypeDef    OBInit;
-    /* Set BFB2 bit to enable boot from Flash Bank2 */
-    /* Allow Access to the Flash control registers and user Flash. */
-    HAL_FLASH_Unlock();
-    /* Allow Access to the option bytes sector. */
-    HAL_FLASH_OB_Unlock();
-    /* Get the Dual boot configuration status. */
-    HAL_FLASHEx_OBGetConfig(&OBInit);
-
-    return ( (OBInit.USERConfig & OB_BFB2_ENABLE) == OB_BFB2_ENABLE ) ? 0 : 1;
-}
-#endif
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
