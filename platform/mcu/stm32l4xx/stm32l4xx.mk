a4faa6ecf69bc532567514737ba62e3a921940df
Fan Qian <pigmike1018@126.com>
2018-03-16 17:45:56 +0800

    Adapt the sd hal interface for stm32l496

--- platform/mcu/stm32l4xx/stm32l4xx.mk
+++ platform/mcu/stm32l4xx/stm32l4xx.mk
@@ -40,7 +40,8 @@ $(NAME)_SOURCES := Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c  \
                    Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c   \
                    Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c    \
                    Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c \
-                   Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c
+                   Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c \
+                   Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_sd.c
 
 $(NAME)_SOURCES += aos/soc_impl.c \
                    aos/trace_impl.c
