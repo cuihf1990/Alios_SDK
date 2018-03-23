0bd96ddb4b53d9743235812c519f1b8c909fa745
Fan Qian <pigmike1018@126.com>
2018-03-23 11:47:11 +0800

    fix the error for cpp_init

--- kernel/cplusplus/cpp_init.c
+++ kernel/cplusplus/cpp_init.c
@@ -7,6 +7,8 @@ typedef void (*cpp_func) ();
 extern cpp_func __ctors_start__[];
 extern cpp_func __ctors_end__[];
 
+__attribute__((weak)) void * __dso_handle = 0;
+
 void cpp_init(void)
 {
     cpp_func *f;
