6c343696f12d27de614f577bcb752914a4bd9592
yuanlu <yuanlu.gl@alibaba-inc.com>
2018-03-25 16:31:22 +0800

    delete unused print & fix iar compile problem & adjust guiapp sensor read interval

--- device/sensor/drv/drv_acc_bosch_bma253.c
+++ device/sensor/drv/drv_acc_bosch_bma253.c
@@ -483,7 +483,7 @@ static int drv_acc_bosch_bma253_ioctl(int cmd, unsigned long arg)
         }break;
         case SENSOR_IOCTL_GET_INFO:{ 
             /* fill the dev info here */
-            dev_sensor_info_t *info =arg;
+            dev_sensor_info_t *info = (dev_sensor_info_t *)arg;
             *(info->model) = "BMA253";
             info->range_max = 16;
             info->range_min = 2;
