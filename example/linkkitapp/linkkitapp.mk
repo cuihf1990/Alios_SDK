803a0407b1b5ec66b85ac56bd2ca064f3f3eedca
桂挺(061101) <ting.guit@alibaba-inc.com>
2018-04-04 06:55:14 -0700

    add linkkit

--- example/linkkitapp/linkkitapp.mk
+++ example/linkkitapp/linkkitapp.mk
@@ -6,7 +6,7 @@ GLOBAL_DEFINES      +=  TEST_02
 
 $(NAME)_SOURCES     := linkkit-example.c linkkit_app.c linkkit_export.c lite_queue.c
 
-$(NAME)_COMPONENTS := protocol.alink-ilop connectivity.mqtt fota netmgr framework.common  protocol.linkkit.cmp protocol.linkkit.dm ywss4linkkit
+$(NAME)_COMPONENTS := protocol.alink-ilop connectivity.mqtt fota netmgr framework.common  protocol.linkkit.cm protocol.linkkit.dm protocol.linkkit.alcs ywss4linkkit
 
 ifeq ($(LWIP),1)
 $(NAME)_COMPONENTS  += protocols.net
